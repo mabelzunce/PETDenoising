c7502889089f7b5e825a82810f8f865980163ee8
MiluLedesma <ledesma.milagrosn@gmail.com>
2022-06-23 14:01:03 -0300

    With input norm

--- cnn/python/trainUnetM.py
+++ cnn/python/trainUnetM.py
@@ -15,22 +15,23 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-from sklearn.model_selection import train_test_split
 from datetime import datetime
 from utils import trainModel
 from utils import reshapeDataSet
 from unetM import Unet
 
-unet = Unet()
 
 # Importo base de datos ...
 path = os.getcwd()
-pathGroundTruth = path+'/groundTruth/100'
+pathGroundTruth = path+'/NewDataset/groundTruth/100'
 arrayGroundTruth = os.listdir(pathGroundTruth)
 trainGroundTruth = []
 validGroundTruth = []
 
+unet = Unet()
+
 ramdomIdx = np.random.randint(1, len(arrayGroundTruth)+1, 2).tolist()
+print(ramdomIdx)
 
 for element in arrayGroundTruth:
     pathGroundTruthElement = pathGroundTruth+'/'+element
@@ -43,7 +44,7 @@ for element in arrayGroundTruth:
     else:
         validGroundTruth.append(noisyDataSet)
 
-pathNoisyDataSet = path+'/noisyDataSet/1'
+pathNoisyDataSet = path+'/NewDataset/noisyDataSet/5'
 arrayNoisyDataSet= os.listdir(pathNoisyDataSet)
 trainNoisyDataSet = []
 validNoisyDataSet = []
@@ -70,42 +71,46 @@ for subject in range(0,len(trainGroundTruth)):
         else:
             norm = ((subjectElement[slice,:,:] / meanSlice))
         trainGroundTruthNorm.append(norm)
+        trainGroundTruthNorm.append(np.rot90(norm))
 
 validGroundTruthNorm = []
 for subject in range(0, len(validGroundTruth)):
     subjectElement = validGroundTruth[subject]
     for slice in range(0, subjectElement.shape[0]):
         meanSlice = subjectElement[slice, :, :].mean()
-        if meanSlice == 0.0:
-            norm = ((subjectElement[slice, :, :]))
+        stdSlice = subjectElement[slice, :, :].std()
+        if stdSlice == 0.0:
+            norm = ((subjectElement[slice, :, :]) - meanSlice)
         else:
-            norm = ((subjectElement[slice, :, :] / meanSlice))
-        trainGroundTruthNorm.append(norm)
+            norm = ((subjectElement[slice, :, :] - meanSlice) / stdSlice)
         validGroundTruthNorm.append(norm)
+        validGroundTruthNorm.append(np.rot90(norm))
 
 trainNoisyDataSetNorm = []
 for subject in range(0, len(trainNoisyDataSet)):
     subjectElement = trainNoisyDataSet[subject]
     for slice in range(0, subjectElement.shape[0]):
         meanSlice = subjectElement[slice, :, :].mean()
-        if meanSlice == 0.0:
-            norm = ((subjectElement[slice, :, :]))
+        stdSlice = subjectElement[slice, :, :].std()
+        if stdSlice == 0.0:
+            norm = ((subjectElement[slice, :, :]) - meanSlice)
         else:
-            norm = ((subjectElement[slice, :, :] / meanSlice))
-        trainGroundTruthNorm.append(norm)
+            norm = ((subjectElement[slice, :, :] - meanSlice) / stdSlice)
         trainNoisyDataSetNorm.append(norm)
+        trainNoisyDataSetNorm.append(np.rot90(norm))
 
 validNoisyDataSetNorm = []
 for subject in range(0, len(validNoisyDataSet)):
     subjectElement = validNoisyDataSet[subject]
     for slice in range(0, subjectElement.shape[0]):
         meanSlice = subjectElement[slice, :, :].mean()
-        if meanSlice == 0.0:
-            norm = ((subjectElement[slice, :, :]))
+        stdSlice = subjectElement[slice, :, :].std()
+        if stdSlice == 0.0:
+            norm = ((subjectElement[slice, :, :]) - meanSlice)
         else:
-            norm = ((subjectElement[slice, :, :] / meanSlice))
-        trainGroundTruthNorm.append(norm)e)
+            norm = ((subjectElement[slice, :, :] - meanSlice) / stdSlice)
         validNoisyDataSetNorm.append(norm)
+        validNoisyDataSetNorm.append(np.rot90(norm))
 
 trainGroundTruthNorm = np.array(trainGroundTruthNorm)
 validGroundTruthNorm = np.array(validGroundTruthNorm)
@@ -132,13 +137,16 @@ optimizer = optim.Adam(unet.parameters(), lr=0.0001)
 lossValuesTraining,lossValuesEpoch, lossValuesDevSet, lossValuesDevSetAllEpoch = trainModel(unet,trainingSet, validSet,criterion,optimizer,4,70)
 
 df = pd.DataFrame(lossValuesTraining)
-df.to_excel('lossValuesTrainingSetBatchModel3.xlsx')
+df.to_excel('lossValuesTrainingSetBatchModel5.xlsx')
 
 df = pd.DataFrame(lossValuesEpoch)
-df.to_excel('lossValuesTrainingSetEpochModel3.xlsx')
+df.to_excel('lossValuesTrainingSetEpochModel5.xlsx')
 
 df = pd.DataFrame(lossValuesDevSet)
-df.to_excel('lossValuesDevSetBatchModel3.xlsx')
+df.to_excel('lossValuesDevSetBatchModel5.xlsx')
 
 df = pd.DataFrame(lossValuesDevSetAllEpoch)
-df.to_excel('lossValuesDevSetEpochModel3.xlsx')
+df.to_excel('lossValuesDevSetEpochModel5.xlsx')
+
+df = pd.DataFrame(ramdomIdx)
+df.to_excel('validSubjects.xlsx')
