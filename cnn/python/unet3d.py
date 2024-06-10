deb0d74ad39b63ebcd4791441bf5184a7c5c2313
MiluLedesma <miluledesma14@gmail.com>
2024-06-09 21:06:46 -0300

    new codes for unet 3d

---
+++ cnn/python/unet3d.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv3d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm3d(out_ch),
+            nn.ReLU(inplace=True),
+            #nn.Dropout(),
+            nn.Conv3d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm3d(out_ch),
+            nn.ReLU(inplace=True))
+            #nn.Dropout())
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class InConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(InConv, self).__init__()
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class Down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool3d(2),
+            DoubleConv(in_ch, out_ch)
+        )
+
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear = True):
+        super(Up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch // 2, in_ch // 2, 2, stride=2)
+
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffZ = x2.size()[2] - x1.size()[2]
+        diffY = x2.size()[3] - x1.size()[3]
+        diffX = x2.size()[4] - x1.size()[4]
+
+        x1 = F.pad(x1, (diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2,
+                        diffZ // 2, diffZ - diffZ // 2))
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+class OutConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv3d(in_ch, out_ch, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class AddResidual(nn.Module):
+    def __init__(self):
+        super(AddResidual, self).__init__()
+        #self.relu = nn.ReLU(inplace=True)
+    def forward(self, x, residual):
+        x = x + residual
+        # Image has only positive values:
+        #x = self.relu(x) # Removing the relu as it is creating too many 0 zero values as negative values are all penalized in the same way.
+        return x
+
+class Unet(nn.Module):
+    def __init__(self, in_channels, classes):
+        super(Unet, self).__init__()
+        self.n_channels = in_channels
+        self.n_classes = classes
+
+        self.inc = InConv(in_channels, 16)
+        self.down1 = Down(16, 32)
+        self.down2 = Down(32, 64)
+        self.down3 = Down(64, 128)
+        self.down4 = Down(128, 256)
+        self.up1 = Up(256+128, 128)
+        self.up2 = Up(128+64, 64)
+        self.up3 = Up(64+32, 32)
+        self.up4 = Up(32+16, 16)
+        self.outc = OutConv(16, classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        return x
+
+
+class UnetWithResidual(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UnetWithResidual, self).__init__()
+        self.n_channels = in_channels
+        self.n_classes = out_channels
+
+        self.inc = InConv(in_channels, 16)
+        self.down1 = Down(16, 32)
+        self.down2 = Down(32, 64)
+        self.down3 = Down(64, 128)
+        self.down4 = Down(128, 256)
+        self.up1 = Up(256 + 128, 128)
+        self.up2 = Up(128 + 64, 64)
+        self.up3 = Up(64 + 32, 32)
+        self.up4 = Up(32 + 16, 16)
+        self.outc = OutConv(16, 1)
+        self.add_res = AddResidual()
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        y4 = self.up1(x5, x4)
+        y3 = self.up2(y4, x3)
+        y2 = self.up3(y3, x2)
+        y1 = self.up4(y2, x1)
+        res = self.outc(y1)
+        y = self.add_res(x, res)
+        return y
